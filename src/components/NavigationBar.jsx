244ad92fb3be0b8373563fd973ef869b4ecb4d21
Ramy Awad <ramyaqt20@gmail.com>
2024-08-07 18:21:53 +0300

    added background blur to navbar on scroll

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="flex justify-between text-white font-bold py-5 sticky top-0 bg-navbar-clr z-50">
+    <nav
+      className={`navbar flex justify-between text-white font-bold py-5 sticky top-0 bg-navbar-clr z-50 
+        ${isScrolled && `blurred`}
+        `}
+    >
       <div className="flex flex-start">
         <Link to="/" className="text-lg">
           <img className="h-16 w-32" src="../images/Logo.png" alt="Logo" />
